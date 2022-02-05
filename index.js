b193728ff9f8d955122104295e22b7468992db78
Jon Kaplon <jon.kaplon1@gmail.com>
2022-02-05 11:54:21 -0800

    commit tree a105e5f65fa6ee10afcd722b0655da5fe6538085

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+function findMatching(arr, string) {
+  const driver = arr.filter((el) => el.toLowerCase() === string.toLowerCase());
+  return driver;
+}
+
+function fuzzyMatch(arr, string) {
+  const driver = arr.filter(
+    (el) => el.toLowerCase().indexOf(string.toLowerCase()) === 0
+  );
+  return driver;
+}
+
+function matchName(arr, string) {
+  const driver = arr.filter(
+    (el) => el.name.toLowerCase() === string.toLowerCase()
+  );
+  return driver;
+}
